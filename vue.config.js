ff9a444bf39cef99278ba7c42cf6909203506353
Vinest1412 <93713077+Vinest1412@users.noreply.github.com>
2021-12-09 12:56:20 +0700

    a

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/vue-kevinagustria/" : "/",
-  };
+    publicPath: process.env.NODE_ENV === "production" ? "/vue-kevinagustria/" : "/"
+}
